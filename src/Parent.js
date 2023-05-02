a77a0aa8f561faf89c89643a4919d7d2cedbc0ed
valeria caliguire <caliguireval@gmail.com>
2023-05-02 17:14:08 -0600

    this is important

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,19 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  const [childrenColor, setChildrenColor] = useState("#FFF")
+
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // update color state to a new value
+    setChildrenColor(newChildColor)
+  }
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color= {childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color= {childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
